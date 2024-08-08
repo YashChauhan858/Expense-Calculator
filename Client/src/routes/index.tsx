e0f7a6fe4f0d48038f8c41e395e0524bd7bbaa1b
Yash Chauhan <forcourse1234@gmail.com>
2024-08-08 16:29:09 +0530

    fixed login page redirect

--- Client/src/routes/index.tsx
+++ Client/src/routes/index.tsx
@@ -14,7 +14,7 @@ function Index() {
         <p className="text-lg mb-6 text-gray-300">
           Manage all your expenses and get beautiful analytics
         </p>
-        <Link to="/login">
+        <Link to="/login-register" search={{ type: "login" }}>
           <button className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
             Login
           </button>
