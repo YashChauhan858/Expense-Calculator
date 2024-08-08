253e5add42ddc22ef97cc705a63bc8adf7822c32
Yash Chauhan <forcourse1234@gmail.com>
2024-08-08 20:42:23 +0530

    displaying global and user tags

--- Client/src/routes/_dashboardLayout/tags.tsx
+++ Client/src/routes/_dashboardLayout/tags.tsx
@@ -1,16 +1,27 @@
 import { CreateTag } from "@/components/tags/CreateTag";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { globalTags } from "@/constant/globalTags";
 export const Route = createFileRoute("/_dashboardLayout/tags")({
   component: Tags,
 });
 
 function Tags() {
+  const [userTag, setUserTag] = useState<{ name: string; color: string }[]>([]);
+
+  const isValidTagList = userTag && userTag.length > 0;
+
   const [createTagModal, setCreateTagModal] = useState(false);
   const openModal = () => setCreateTagModal(true);
   const closeModal = () => setCreateTagModal(false);
+
+  useEffect(() => {
+    setUserTag([{ name: "Netflix", color: "#FFD700" }]);
+    return () => {};
+  }, []);
+
   return (
     <section className="pt-10">
       <h1 className="text-2xl font-bold">Manage Your Tags</h1>
@@ -22,7 +33,38 @@ function Tags() {
         Create Tag
       </Button>
       {createTagModal && <CreateTag close={closeModal} />}
-      <div></div>
+      <div className="mt-5">
+        <h3 className="text-lg">Global Tags</h3>
+        <div className="flex flex-wrap gap-3 mt-5 w-[90%]">
+          {globalTags.map((tags) => (
+            <Badge
+              variant="outline"
+              style={{ backgroundColor: tags?.color }}
+              className="text-black select-none"
+              key={tags?.name}
+            >
+              {tags?.name}
+            </Badge>
+          ))}
+        </div>
+      </div>
+      <div className="mt-5">
+        <h3 className="text-lg">Your Tags</h3>
+        <div className="flex flex-wrap gap-3 mt-5 w-[90%]">
+          {isValidTagList &&
+            userTag.map((tags) => (
+              <Badge
+                variant="outline"
+                style={{ backgroundColor: tags?.color }}
+                className="text-black select-none"
+                key={tags?.name}
+              >
+                {tags?.name}
+              </Badge>
+            ))}
+          {!isValidTagList && <p className="text-sm">No Tags Created Yet</p>}
+        </div>
+      </div>
     </section>
   );
 }
