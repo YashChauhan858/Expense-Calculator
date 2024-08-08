ba4fd48f5636a9035e457f89da733b4f77813b75
Yash Chauhan <forcourse1234@gmail.com>
2024-08-08 20:42:08 +0530

    added global tag list

---
+++ Client/src/constant/globalTags.ts
@@ -0,0 +1,25 @@
+import colors from "./colors.json";
+const lightColors = colors.lightColors;
+const darkColors = colors.darkColors;
+export const globalTags = [
+  { name: "Food & Dining", color: lightColors[0] },
+  { name: "Transportation", color: lightColors[1] },
+  { name: "Utilities", color: lightColors[2] },
+  { name: "Rent/Mortgage", color: lightColors[3] },
+  { name: "Entertainment", color: lightColors[4] },
+  { name: "Healthcare", color: lightColors[5] },
+  { name: "Groceries", color: lightColors[6] },
+  { name: "Shopping", color: lightColors[7] },
+  { name: "Travel", color: lightColors[8] },
+  { name: "Education", color: lightColors[9] },
+  { name: "Insurance", color: darkColors[0] },
+  { name: "Savings", color: darkColors[1] },
+  { name: "Investments", color: darkColors[2] },
+  { name: "Gifts & Donations", color: darkColors[3] },
+  { name: "Personal Care", color: darkColors[4] },
+  { name: "Debt Payments", color: darkColors[5] },
+  { name: "Taxes", color: darkColors[6] },
+  { name: "Income", color: darkColors[7] },
+  { name: "Miscellaneous", color: darkColors[8] },
+  { name: "Subscriptions", color: darkColors[9] },
+];
